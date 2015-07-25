de77e2fcc86fe3f2b6791770bf814f5f40802281
Paul Kim <kmc5500@naver.com>
2015-07-25 22:03:17 +0900

    ex7-18

---
+++ chap7/src/ex7_17/Marine.java
@@ -0,0 +1,8 @@
+package ex7_17;
+
+public class Marine extends Unit{
+	
+	void stimPack(){
+		System.out.println("Snaaaaheeeeaaaa~~~!!");
+	}
+}
---
+++ chap7/src/ex7_17/Tank.java
@@ -0,0 +1,8 @@
+package ex7_17;
+
+public class Tank extends Unit{
+	void chageMode(){
+		System.out.print("Change mode!");
+	}
+
+}
